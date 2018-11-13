28b3743909716eb85e1e2f9eeef5a604173d15c0
UttamBhandari <uttambhandari75@gmail.com>
2018-11-13 13:24:34 +0545

    Read Data from a file

---
+++ Data Analysis Software/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Data_Analysis_Software
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                string text = System.IO.File.ReadAllText(file);
+
+                Console.WriteLine(text);    
+            }
+        }
+    
+    }
+}
