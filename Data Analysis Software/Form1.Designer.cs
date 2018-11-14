58fe07d56d22f8359cdfccdc0f85d574e33ea177
UttamBhandari <uttambhandari75@gmail.com>
2018-11-14 16:36:39 +0545

    Data calculated and displayed

--- Data Analysis Software/Form1.Designer.cs
+++ Data Analysis Software/Form1.Designer.cs
@@ -32,6 +32,14 @@
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.labelstarttime = new System.Windows.Forms.Label();
+            this.labelweight = new System.Windows.Forms.Label();
+            this.labeldate = new System.Windows.Forms.Label();
+            this.labellength = new System.Windows.Forms.Label();
+            this.labelsmode = new System.Windows.Forms.Label();
+            this.labelmonitor = new System.Windows.Forms.Label();
+            this.labelinterval = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,7 +64,7 @@
             // openFileToolStripMenuItem
             // 
             this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
-            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
             this.openFileToolStripMenuItem.Text = "Open file";
             this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
             // 
@@ -64,11 +72,90 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(281, 43);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(494, 382);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "Start Time";
+            // 
+            // labelstarttime
+            // 
+            this.labelstarttime.AutoSize = true;
+            this.labelstarttime.Location = new System.Drawing.Point(25, 46);
+            this.labelstarttime.Name = "labelstarttime";
+            this.labelstarttime.Size = new System.Drawing.Size(55, 13);
+            this.labelstarttime.TabIndex = 3;
+            this.labelstarttime.Text = "Start Time";
+            // 
+            // labelweight
+            // 
+            this.labelweight.AutoSize = true;
+            this.labelweight.Location = new System.Drawing.Point(26, 199);
+            this.labelweight.Name = "labelweight";
+            this.labelweight.Size = new System.Drawing.Size(41, 13);
+            this.labelweight.TabIndex = 13;
+            this.labelweight.Text = "Weight";
+            // 
+            // labeldate
+            // 
+            this.labeldate.AutoSize = true;
+            this.labeldate.Location = new System.Drawing.Point(25, 148);
+            this.labeldate.Name = "labeldate";
+            this.labeldate.Size = new System.Drawing.Size(30, 13);
+            this.labeldate.TabIndex = 12;
+            this.labeldate.Text = "Date";
+            // 
+            // labellength
+            // 
+            this.labellength.AutoSize = true;
+            this.labellength.Location = new System.Drawing.Point(26, 171);
+            this.labellength.Name = "labellength";
+            this.labellength.Size = new System.Drawing.Size(40, 13);
+            this.labellength.TabIndex = 10;
+            this.labellength.Text = "Length";
+            // 
+            // labelsmode
+            // 
+            this.labelsmode.AutoSize = true;
+            this.labelsmode.Location = new System.Drawing.Point(25, 126);
+            this.labelsmode.Name = "labelsmode";
+            this.labelsmode.Size = new System.Drawing.Size(41, 13);
+            this.labelsmode.TabIndex = 11;
+            this.labelsmode.Text = "SMode";
+            // 
+            // labelmonitor
+            // 
+            this.labelmonitor.AutoSize = true;
+            this.labelmonitor.Location = new System.Drawing.Point(25, 102);
+            this.labelmonitor.Name = "labelmonitor";
+            this.labelmonitor.Size = new System.Drawing.Size(42, 13);
+            this.labelmonitor.TabIndex = 9;
+            this.labelmonitor.Text = "Monitor";
+            // 
+            // labelinterval
+            // 
+            this.labelinterval.AutoSize = true;
+            this.labelinterval.Location = new System.Drawing.Point(25, 76);
+            this.labelinterval.Name = "labelinterval";
+            this.labelinterval.Size = new System.Drawing.Size(42, 13);
+            this.labelinterval.TabIndex = 8;
+            this.labelinterval.Text = "Interval";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelweight);
+            this.Controls.Add(this.labeldate);
+            this.Controls.Add(this.labellength);
+            this.Controls.Add(this.labelsmode);
+            this.Controls.Add(this.labelmonitor);
+            this.Controls.Add(this.labelinterval);
+            this.Controls.Add(this.labelstarttime);
+            this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -86,6 +173,14 @@
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label labelstarttime;
+        private System.Windows.Forms.Label labelweight;
+        private System.Windows.Forms.Label labeldate;
+        private System.Windows.Forms.Label labellength;
+        private System.Windows.Forms.Label labelsmode;
+        private System.Windows.Forms.Label labelmonitor;
+        private System.Windows.Forms.Label labelinterval;
     }
 }
 
--- Data Analysis Software/Form1.cs
+++ Data Analysis Software/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,23 +13,60 @@ namespace Data_Analysis_Software
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, string> _HRdata = new Dictionary<string, string>();
+        private Dictionary<string, string> _parameter = new Dictionary<string, string>();
         public Form1()
         {
             InitializeComponent();
         }
+        private static string[] SplitData(string text)
+        {
 
+
+            var split = new string[] { "[Params]", "[Note]", "[IntTimes]",
+                "[IntNotes]", "[ExtraData]", "[LapNames]", "[Summary-123]",
+                "[Summary-TH]", "[HRZones]", "[SwapTimes]", "[Trip]", "[HRData]" };
+            var splitted = text.Split(split, StringSplitOptions.RemoveEmptyEntries);
+            return splitted;
+
+        }
+        private static string[] SplitDataByEnter(string text)
+        {
+            return text.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        }
         private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
         {
             DialogResult result = openFileDialog1.ShowDialog();
 
             if (result == DialogResult.OK)
             {
-                string file = openFileDialog1.FileName;
-                string text = System.IO.File.ReadAllText(file);
-
-                Console.WriteLine(text);    
+                _parameter = new Dictionary<string, string>();
+                string text = File.ReadAllText(openFileDialog1.FileName);
+                var splittedString = SplitData(text);
+                var splittedParaData = SplitDataByEnter(splittedString[0]);
+                foreach (var data in splittedParaData)
+                {
+                    if (data != "\r")
+                    {
+                        string[] parts = data.Split(new[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+                        _parameter.Add(parts[0], parts[1]);
+                    }
+                }
+                foreach (var a in _parameter)
+                {
+                    Console.WriteLine(a.Key);
+                }
+                //StreamReader sr = new StreamReader(Application.StartupPath +);
+                //richTextBox2.Text = sr.ReadToEnd();
+                //sr.Close();
+                labelstarttime.Text = labelstarttime.Text + "= " + _parameter["StartTime"];
+                labelinterval.Text = labelinterval.Text + "= " + _parameter["Interval"];
+                labelmonitor.Text = labelmonitor.Text + "= " + _parameter["Monitor"];
+                labelsmode.Text = labelsmode.Text + "= " + _parameter["SMode"];
+                labeldate.Text = labeldate.Text + "= " + _parameter["Date"];
+                labellength.Text = labellength.Text + "= " + _parameter["Length"];
+                labelweight.Text = labelweight.Text + "= " + _parameter["Weight"];
             }
         }
-    
     }
 }
